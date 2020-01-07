345036c6861e8e0030435905179b80c47f6880f7
Berthier Esteban <44427849+berthieresteban@users.noreply.github.com>
2020-01-07 11:20:41 +0100

    add dashboard scss files and bootstrap app (#6)
    * add dashboard scss files and bootstrap app
    
    * emulate realtime

---
+++ frontend/src/Container/SideBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-navigation-drawer app expand-on-hover permanent dark>
+    <template v-slot:prepend>
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link two-line>
+          <v-list-item-content>
+            <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </template>
+
+    <v-divider></v-divider>
+
+    <v-list nav dense>
+      <v-list-item link :to="`/CryptoDetails/${cur.currency}`" v-for="cur of cryptoCurrency" :key="cur.currency">
+        <v-list-item-icon>
+          <v-icon>{{cur.icon}}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>
+          <span class="overline text-left">{{cur.value}}</span>
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          <span class="subtitle-2 text-right green--text">{{cur.percentage}}%</span>
+          <v-icon dense v-if="cur.percentage > 0" color="#2E7D32">fa-arrow-up</v-icon>
+          <v-icon dense v-if="cur.percentage === 0" color="#BDBDBD">fa-arrow-right</v-icon>
+          <v-icon dense v-if="cur.percentage < 0" color="#C62828">fa-arrow-down</v-icon>
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cryptoCurrency: [
+        {
+          currency: 'BTC',
+          icon: 'fab fa-bitcoin',
+          value: '3232.234',
+          percentage: '0.98'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
