a0bd1c111b0ed21c8531cfb7b78fbf2fbfc76a40
Arnaud Brown <arnaud.brown@epitech.eu>
2020-01-20 10:06:11 +0100

    simple crypto

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,18 +3,24 @@ var router = express.Router();
 const client = require('../config/clientPg')
 
 
-let query = 'select * from CRYPTO_LIST'
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  client
-    .query(query)
-    .then(result => {
-      res.json({
-        role: "anomyme",
-        test: result.rows
-      })
-    })
-    .catch(e => console.error(e.stack))
+router.get('/cryptos', function (req, res, next) {
+    let query = "SELECT * from CRYPTO_LIST"
+    if (req.query.cmids != undefined) {
+        const cmids = req.query.cmids.split(",");
+        let values = "'" + cmids[0] + "'"
+        for (let i = 1; i < cmids.length; i++)
+            values += ",'" + cmids[i] + "'"
+        query = 'SELECT * from CRYPTO_LIST WHERE fullName = ' + values + ''
+    }
+    client
+        .query(query)
+        .then(result => {
+            res.json({
+                test: result.rows
+            })
+        })
+        .catch(e => console.error(e.stack))
 });
 
 module.exports = router;
--- backend/scripts/db_sample.sql
+++ backend/scripts/db_sample.sql
@@ -3,90 +3,89 @@ CREATE DATABASE money;
 \c money;
 DROP TABLE IF EXISTS RSS_LIST CASCADE;
 CREATE TABLE RSS_LIST (
-    id serial PRIMARY KEY,
-    link VARCHAR(500) NOT NULL,
-    description TEXT
+ id serial PRIMARY KEY,
+ link VARCHAR(500) NOT NULL,
+ description TEXT
 );
-
 DROP TABLE IF EXISTS CRYPTO_LIST CASCADE;
 CREATE TABLE CRYPTO_LIST (
-    id serial PRIMARY KEY,
-    name VARCHAR(500)
+ id serial PRIMARY KEY,
+ symbol VARCHAR(6),
+ fullName VARCHAR(500),
+ picture_url TEXT
 );
-
 DROP TABLE IF EXISTS CRYPTO_HISTORY CASCADE;
 CREATE TABLE CRYPTO_HISTORY (
-    id serial PRIMARY KEY,
-    crypto_id BIGINT NOT NULL,
-    timestamp TIMESTAMP,
-    open FLOAT,
-    high FLOAT,
-    low FLOAT,
-    close FLOAT,
-    FOREIGN KEY (crypto_id) REFERENCES CRYPTO_LIST
+ id serial PRIMARY KEY,
+ crypto_id BIGINT NOT NULL,
+ period VARCHAR(10),
+ timestamp TIMESTAMP,
+ price FLOAT,
+ open FLOAT,
+ high FLOAT,
+ low FLOAT,
+ close FLOAT,
+ FOREIGN KEY (crypto_id) REFERENCES CRYPTO_LIST
 );
 SET timezone = 'America/Los_Angeles';
-
 DROP TABLE IF EXISTS USERS CASCADE;
 CREATE TABLE USERS (
-    id serial PRIMARY KEY,
-    is_admin BOOLEAN NOT NULL,
-    email VARCHAR(500) NOT NULL,
-    password VARCHAR(500) NOT NULL,
-    username VARCHAR(500) NOT NULL,
-    picture_url TEXT,
-    keyword VARCHAR(500) [],
-    favorites_crypto BIGINT []
+ id serial PRIMARY KEY,
+ is_admin BOOLEAN NOT NULL,
+ email VARCHAR(500) NOT NULL,
+ password VARCHAR(500) NOT NULL,
+ username VARCHAR(500) NOT NULL,
+ picture_url TEXT,
+ keyword VARCHAR(500) [],
+ favorites_crypto BIGINT []
 );
-
 INSERT INTO CRYPTO_LIST VALUES (
-    nextval('crypto_list_id_seq'),
-    'bitcoin'
+nextval('crypto_list_id_seq'),
+'BTC', 'BitCoin', '/url/picture'
 );
-
 INSERT INTO CRYPTO_LIST VALUES (
-    nextval('crypto_list_id_seq'),
-    'etherum'
+nextval('crypto_list_id_seq'),
+'ETC', 'Eterum', '/url/picture2'
 );
-
-INSERT INTO CRYPTO_HISTORY VALUES (
-    nextval('crypto_history_id_seq'),
-    1,
-    '2016-06-22 19:10:25-07',
-    425235,
-    645646,
-    41985298,
-    5252895
-);
-
-INSERT INTO CRYPTO_HISTORY VALUES (
-    nextval('crypto_history_id_seq'),
-    2,
-    '2016-06-22 19:10:25-07',
-    87765,
-    09876,
-    12457689,
-    6565678
-);
-
+-- INSERT INTO CRYPTO_LIST VALUES (
+-- nextval('crypto_list_id_seq'),
+-- 'etherum'
+-- );
+-- INSERT INTO CRYPTO_HISTORY VALUES (
+-- nextval('crypto_history_id_seq'),
+-- 1,
+-- '2016-06-22 19:10:25-07',
+-- 425235,
+-- 645646,
+-- 41985298,
+-- 5252895
+-- );
+-- INSERT INTO CRYPTO_HISTORY VALUES (
+-- nextval('crypto_history_id_seq'),
+-- 2,
+-- '2016-06-22 19:10:25-07',
+-- 87765,
+-- 09876,
+-- 12457689,
+-- 6565678
+-- );
 INSERT INTO USERS VALUES (
-    nextval('users_id_seq'),
-    false,
-    'yoann.abbes@gmail.com',
-    'testmdp',
-    'testuser',
-    'http://osef.com',
-    ARRAY ['bitcoin', 'etherum'],
-    ARRAY [1, 2]
+ nextval('users_id_seq'),
+ false,
+ 'yoann.abbes@gmail.com',
+ 'testmdp',
+ 'testuser',
+ 'http://osef.com',
+ ARRAY ['bitcoin', 'etherum'],
+ ARRAY [1, 2]
 );
-
 INSERT INTO USERS VALUES (
-    nextval('users_id_seq'),
-    false,
-    'yoann1.abbes@epitech.eu',
-    'testmdp2',
-    'testuser2',
-    'http://osef2.com',
-    ARRAY ['bitcoin', 'etherum'],
-    ARRAY [1, 2]
+ nextval('users_id_seq'),
+ false,
+ 'yoann1.abbes@epitech.eu',
+ 'testmdp2',
+ 'testuser2',
+ 'http://osef2.com',
+ ARRAY ['bitcoin', 'etherum'],
+ ARRAY [1, 2]
 );
