fe0cd593f9d55e146d6b877944aa084cdb37c651
maximegavens <55391608+maximegavens@users.noreply.github.com>
2020-01-27 08:56:38 +0100

    user login (#18)
    template user login
    with facebook login (don't work again)

---
+++ frontend/src/LogIn/LogIn.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col md="4" offset-md="4">
+        <v-img
+          alt="BITSOFCOIN"
+          class="text-xs-center"
+          src="@/core/assets/logo3.png"
+          width="420">
+        </v-img>
+        <v-card width="420">
+          <v-card-title class="headline">
+            Login Here
+          </v-card-title>
+          <v-card-text>
+            <form @submit.prevent="loginUser">
+              <v-text-field
+                v-model="email"
+                label="Email"
+                color="secondary"
+                required>
+              </v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                color="secondary"
+                type="password"
+                required>
+              </v-text-field>
+              <v-btn
+                color="secondary"
+                type="submit">
+                Log In
+              </v-btn>
+              Want to Register? Register <a href="/SignUp">
+              here
+            </a>
+            </form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginUser () {
+      let target = {
+        email: this.email,
+        password: this.password
+      }
+      console.log(target)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
