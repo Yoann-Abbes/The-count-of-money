162fa2e9ef977979d8a6ee232789f84ca78c963c
Berthier Esteban <44427849+berthieresteban@users.noreply.github.com>
2020-01-27 08:57:25 +0100

    add logs to bootstrap and add cronFetcher script (#13)
    What does this PR do?
    This PR add cronFetcher.js script that fetch for each crypto currency referenced in crypto_list table:
    
    the last minut every minut and delete the oldest
    the last hour every hour and delete the oldest
    the last day every day and delete the oldest
    How should this be manually tested?
    You'll need the api key for cryptocompare api (teams).
    You can set it in API_KEY env variable or with the --api_key blablabla parameter.
    
    cd backend
    npm i
    cd scripts
    ./init-db.sh
    node bootstrap.js (you can see the beautiful logs... 😄)
    node cronFetcher.js
    Logs will tells you when new rows are add/removed from tables
    You can verify the changes from your postgres console (psql -h localhost -U postgres)

--- backend/scripts/bootstrap.js
+++ backend/scripts/bootstrap.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { Client } = require('pg');
+const cliProgress = require('cli-progress');
 
 const argv = require('yargs')
     .usage('Usage: You need to run the init-db.sh script to clear the database before run this one to populate it.')
@@ -16,112 +17,133 @@ const client = new Client({
     database: argv.database,
     password: argv.password,
     port: argv.port
-})
+});
 
-client.connect()
-
-const cryptos = ['BTC', 'ETH', 'XRP', 'LTC', 'BCH', 'USDT', 'EOS', 'BNB', 'BSV', 'TRX']
-const symbol = 'EUR'
-const api_key = argv.api_key;
+const SELECTED_CRYPTOS = ['BTC', 'ETH', 'XRP', 'LTC', 'BCH', 'USDT', 'EOS', 'BNB', 'BSV', 'TRX'];
+const DEVISE = 'EUR';
+const APIKEY = argv.api_key;
+const NUMBER_OF_DAYS = 60;
+const NUMBER_OF_HOURS = 48;
+const NUMBER_OF_MINUTES = 120;
 
 // GET ALL CRYPTOS INFOS
 const getCryptoList = async () => {
-    const url = `https://min-api.cryptocompare.com/data/all/coinlist`;
-    let query = 'INSERT INTO crypto_list (symbol, fullname, picture_url) VALUES\n';
-    await axios.get(url)
-        .then(response => {
-            let imageUrl, fullName;
-            for (const crypto of cryptos) {
-                imageUrl = `https://www.cryptocompare.com/${response.data.Data[crypto].ImageUrl}`
-                fullName = response.data.Data[crypto].CoinName
-                query += `('${crypto}', '${fullName}', '${imageUrl}'),\n`
-            }
-        })
-    query = query.slice(0, -2);
-    query += ';'
+    const coinListUrl = `https://min-api.cryptocompare.com/data/all/coinlist`;
+    let cryptoListQuery = 'INSERT INTO crypto_list (symbol, fullname, picture_url) VALUES\n';
+    let imageUrl, fullName;
 
-    await client
-        .query(query)
-        .catch(e => console.error(e.stack))
-}
+    const cryptoListBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    cryptoListBar.start(SELECTED_CRYPTOS.length, 0);
+
+    const cryptoListResponse = await axios.get(coinListUrl);
+    for (const [index, crypto] of SELECTED_CRYPTOS.entries()) {
+        cryptoListBar.update(index + 1);
+        imageUrl = `https://www.cryptocompare.com/${cryptoListResponse.data.Data[crypto].ImageUrl}`;
+        fullName = cryptoListResponse.data.Data[crypto].CoinName;
+        cryptoListQuery += `('${crypto}', '${fullName}', '${imageUrl}'),\n`;
+    }
+    cryptoListQuery = cryptoListQuery.slice(0, -2);
+    cryptoListQuery += ';';
+
+    await client.query(cryptoListQuery);
+    cryptoListBar.stop();
+};
 
 // GET EACH CRYPTO VALUES FOR 60 LAST DAYS
 const getCryptosDays = async () => {
-    let query = 'INSERT INTO crypto_history (crypto_id, period, timestamp, open, high, low, close) VALUES \n';
-    for (const [index, crypto] of cryptos.entries()) {
-        const url = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${crypto}&tsym=${symbol}&limit=60&api_key=${api_key}`;
-        await axios.get(url)
-            .then(response => {
-                const days = response.data.Data.Data;
-                for (const day of days) {
-                    query += `('${index + 1}', 'days', to_timestamp(${day.time}), '${day.open}', '${day.high}', '${day.low}', '${day.close}'),\n`
-                }
+    let cryptoDaysQuery = 'INSERT INTO crypto_history (crypto_id, period, timestamp, open, high, low, close) VALUES \n';
 
-            })
+    const cryptoDaysBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    cryptoDaysBar.start(NUMBER_OF_DAYS * SELECTED_CRYPTOS.length, 0);
+
+    for (const [index, crypto] of SELECTED_CRYPTOS.entries()) {
+        const cryptoDaysUrl = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${crypto}&tsym=${DEVISE}&limit=${NUMBER_OF_DAYS}&api_key=${APIKEY}`;
+        const cryptoDaysResponse = await axios.get(cryptoDaysUrl);
+        const days = cryptoDaysResponse.data.Data.Data;
+        for (const [indexDay, day] of days.entries()) {
+            cryptoDaysBar.update(indexDay + index * NUMBER_OF_DAYS);
+            cryptoDaysQuery += `('${index + 1}', 'daily', to_timestamp(${day.time}), '${day.open}', '${day.high}', '${day.low}', '${day.close}'),\n`;
+        }
     }
-    query = query.slice(0, -2);
-    query += ';'
+    cryptoDaysQuery = cryptoDaysQuery.slice(0, -2);
+    cryptoDaysQuery += ';';
 
-    await client
-        .query(query)
-        .catch(e => console.error(e.stack))
+    await client.query(cryptoDaysQuery);
+    cryptoDaysBar.stop();
 };
 
 
 // GET EACH CRYPTO VALUES FOR 48 LAST HOURS
 const getCryptosHours = async () => {
-    let query = 'INSERT INTO crypto_history (crypto_id, period, timestamp, open, high, low, close) VALUES \n';
-    for (const [index, crypto] of cryptos.entries()) {
-        const url = `https://min-api.cryptocompare.com/data/v2/histohour?fsym=${crypto}&tsym=${symbol}&limit=48&api_key=${api_key}`;
-        await axios.get(url)
-            .then(response => {
-                const hours = response.data.Data.Data;
-                for (const hour of hours) {
-                    date = new Date(hour.time)
-                    query += `('${index + 1}', 'hours', to_timestamp(${hour.time}), '${hour.open}', '${hour.high}', '${hour.low}', '${hour.close}'),\n`
-                }
+    let cryptoHoursQuery = 'INSERT INTO crypto_history (crypto_id, period, timestamp, open, high, low, close) VALUES \n';
 
-            })
+    const cryptoHoursBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    cryptoHoursBar.start(NUMBER_OF_HOURS * SELECTED_CRYPTOS.length, 0);
+
+    for (const [index, crypto] of SELECTED_CRYPTOS.entries()) {
+        const cryptoHoursUrl = `https://min-api.cryptocompare.com/data/v2/histohour?fsym=${crypto}&tsym=${DEVISE}&limit=${NUMBER_OF_HOURS}&api_key=${APIKEY}`;
+        const cryptoHoursResponse = await axios.get(cryptoHoursUrl);
+        const hours = cryptoHoursResponse.data.Data.Data;
+        for (const [indexHour, hour] of hours.entries()) {
+            cryptoHoursBar.update(indexHour + index * NUMBER_OF_HOURS);
+            cryptoHoursQuery += `('${index + 1}', 'hourly', to_timestamp(${hour.time}), '${hour.open}', '${hour.high}', '${hour.low}', '${hour.close}'),\n`;
+        }
+
     }
-    query = query.slice(0, -2);
-    query += ';'
+    cryptoHoursQuery = cryptoHoursQuery.slice(0, -2);
+    cryptoHoursQuery += ';';
 
-    await client
-        .query(query)
-        .catch(e => console.error(e.stack))
+    await client.query(cryptoHoursQuery);
+    cryptoHoursBar.stop();
 };
 
 
 // GET EACH CRYPTO VALUES FOR 120 LAST MINUTS
 const getCryptosMinuts = async () => {
-    let query = 'INSERT INTO crypto_history (crypto_id, period, timestamp, open, high, low, close) VALUES \n';
-    for (const [index, crypto] of cryptos.entries()) {
-        const url = `https://min-api.cryptocompare.com/data/v2/histominute?fsym=${crypto}&tsym=${symbol}&limit=120&api_key=${api_key}`;
-        await axios.get(url)
-            .then(response => {
-                const minuts = response.data.Data.Data;
-                for (const minut of minuts) {
-                    query += `('${index + 1}', 'minuts', to_timestamp(${minut.time}), '${minut.open}', '${minut.high}', '${minut.low}', '${minut.close}'),\n`
-                }
-            })
-    }
-    query = query.slice(0, -2);
-    query += ';'
+    let cryptoMinutsQuery = 'INSERT INTO crypto_history (crypto_id, period, timestamp, open, high, low, close) VALUES \n';
 
-    await client
-        .query(query)
-        .catch(e => console.error(e.stack))
+    const cryptoMinutsBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    cryptoMinutsBar.start(NUMBER_OF_MINUTES * SELECTED_CRYPTOS.length, 0);
+
+    for (const [index, crypto] of SELECTED_CRYPTOS.entries()) {
+        const cryptoMinutsUrl = `https://min-api.cryptocompare.com/data/v2/histominute?fsym=${crypto}&tsym=${DEVISE}&limit=${NUMBER_OF_MINUTES}&api_key=${APIKEY}`;
+        const cryptoMinutsResponse = await axios.get(cryptoMinutsUrl);
+        const minutes = cryptoMinutsResponse.data.Data.Data;
+        for (const [indexMinut, minute] of minutes.entries()) {
+            cryptoMinutsBar.update(indexMinut + index * NUMBER_OF_MINUTES);
+            cryptoMinutsQuery += `('${index + 1}', 'minute', to_timestamp(${minute.time}), '${minute.open}', '${minute.high}', '${minute.low}', '${minute.close}'),\n`;
+        }
+    }
+    cryptoMinutsQuery = cryptoMinutsQuery.slice(0, -2);
+    cryptoMinutsQuery += ';';
+
+    await client.query(cryptoMinutsQuery);
+    cryptoMinutsBar.stop();
 };
 
 (async () => {
     try {
+        client.connect();
+
+        console.log('Fetching crypto Informations...');
         await getCryptoList();
+        console.log('Crypto Informations successfully loaded!\n');
+
+        console.log(`Fetching crypto values of last ${NUMBER_OF_DAYS} days...`);
         await getCryptosDays();
+        console.log('Crypto Days successfully loaded!\n');
+
+        console.log(`Fetching crypto values of last ${NUMBER_OF_HOURS} hours...`);
         await getCryptosHours();
+        console.log('Crypto Hours successfully loaded!\n');
+
+        console.log(`Fetching crypto values of last ${NUMBER_OF_MINUTES} minutes...`);
         await getCryptosMinuts();
+        console.log('Crypto Minutes successfully loaded!');
+
     } catch (error) {
         console.log(error.message);
     } finally {
-        client.end()
+        client.end();
     }
 })();
