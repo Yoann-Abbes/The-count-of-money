dff69a877b0d45b07385bfe8dd02854bb9b1e9a7
maxime gavens <maxime.gavens@epitech.eu>
2020-02-03 16:15:12 +0100

    hello world

--- frontend/src/core/store/auth.js
+++ frontend/src/core/store/auth.js
@@ -1,8 +1,20 @@
+import requester from '../service/requester'
+
 const state = {
   waitingForConnection: false
 }
 
 const actions = {
+  async create ({ dispatch, rootGetters }, user) {
+    const ApiUrl = rootGetters['app/getBaseUrl'] + '/users/register'
+    try {
+      const responseCreate = await requester.post(ApiUrl, user)
+      console.log(responseCreate)
+      return { status: true, message: 'OK' }
+    } catch (error) {
+      return { status: false, message: error.message }
+    }
+  }
 }
 
 const mutations = {
