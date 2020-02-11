5f1e7a22308108cbb30fc14e7739d27bcf0ed5da
Berthier <esteban.berthier@epitech.eu>
2020-02-11 15:50:17 +0100

    fix e2e test

--- frontend/cypress/integration/examples/connect.spec.js
+++ frontend/cypress/integration/examples/connect.spec.js
@@ -4,10 +4,10 @@ context('Actions', () => {
   beforeEach(() => {
   })
 
-  it('should connect to frontend and go to bitcoin currency details', () => {
+  it('should connect to frontend and rss feeds', () => {
     cy.visit('http://localhost:8080/')
     cy.wait(10000)
-    cy.get('a[href*="/CryptoDetails/BTC"]').click()
-    cy.get('.apexcharts-svg')
+    cy.get('a[href*="#/RssFlows"]').click()
+    cy.contains('RSS FEEDS')
   })
 })
